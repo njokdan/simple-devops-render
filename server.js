cf0de2edf61a470c872d161e207cf90854bb8358
njokdan <njokdan@gmail.com>
2026-04-18 09:38:21 +0100

    pushed workflow update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from Darey.io DevOps Demo! hello</h1>
+    <h1>Hello from Darey.io DevOps Demo! hello Team</h1>
     
     `);
 });
