f1953371450a1672dd02046eb782e670a2a6ce2a
njokdan <njokdan@gmail.com>
2026-04-18 08:33:27 +0100

    First push of demo app

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from Darey.io DevOps Demo! </h1>
+    
+    `);
+});
+//🚀</h1>
+//<p>Current time: ${new Date().toISOString()}</p>
+//<p>This app was deployed automatically using Docker + GitHub Actions + Render</p>
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
